74b6c5d285a1fa777be90c2584c9911b4a660c7a
Michael <michaeldchan06@gmail.com>
2023-02-04 08:33:56 -1000

    Cont. updating of alignment methods for cube node

---
+++ src/main/java/frc/robot/Align.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+import org.opencv.core.Mat;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+
+/** Add your docs here. */
+public class Align {
+    Apriltags apriltags = new Apriltags();
+    Drive drive = new Drive();
+    ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
+    // public void Alignment() {
+    //     // Set to top pipeline
+    //     apriltags.setPipeline(1);
+    //     drive.angleAlign();
+    //     // Set to bottom pipeline
+    //     apriltags.setPipeline(2);
+
+    //     // Convert continous angle to limited to 360
+    //     double currentPosition = gyro.getAngle();
+    //     if (gyro.getAngle() > 360) {
+    //         int number = (int) (gyro.getAngle() / 360); 
+    //         currentPosition = gyro.getAngle() - (360 * number);
+    //     }
+    //     if (gyro.getAngle() < 0) {
+    //         int number = (int) (gyro.getAngle() / 360); 
+    //         currentPosition = gyro.getAngle() + (360 * number);
+    //     }
+
+    //     // Calculations for side to side distance.
+    //     double totalAngle = 90 - currentPosition;
+    //     double angle2 = 180 - 90 - totalAngle;
+    //     double angle3 = totalAngle - apriltags.getHorizontalDegToTarget();
+    //     double angleOffset = apriltags.getHorizontalDegToTarget() * Math.PI/180;
+    //     double angle2radians = angle2 * Math.PI/180;
+    //     double angle3radians = angle3 * Math.PI/180;
+
+    //     double length = 17 * Math.sin(angle2radians) / Math.sin(angleOffset);
+    //     double distance = Math.cos(angle3) * length;
+
+    //     // Actual movement
+    //     drive.turnDrive(5000, totalAngle, 20);
+    //     drive.encoderDrive(30, distance, "forward", 5000);
+    //     drive.turnDrive(5000, -90, 20);
+
+    // }
+
+    public void align2() {
+        apriltags.setPipeline(1);
+        drive.angleAlign();
+        double length1 = apriltags.estimateVerticalDistance();
+
+        apriltags.setPipeline(2);
+        double length2 = apriltags.estimateVerticalDistance();
+
+        double offset = apriltags.getHorizontalDegToTarget();
+        double angle1 = Math.asin(Math.sin(offset * length2)/length1);
+
+        double angle2 = 180 - 90 - angle1;
+        double distance = length1/ Math.tan(angle2);
+
+        drive.turnDrive(5000, 90, 0.2);
+        drive.encoderDrive(0.3, distance, "forward", 5000);
+        drive.turnDrive(5000, 180 - angle2, 0.2);
+    }
+}
