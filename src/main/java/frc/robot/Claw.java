5d92fed175bb3c16c4dfad16df5df973dae15b25
Michael <michaeldchan06@gmail.com>
2023-02-18 11:57:55 -1000

    Organized and revised claw and pivot arm code

--- src/main/java/frc/robot/Claw.java
+++ src/main/java/frc/robot/Claw.java
@@ -20,13 +20,9 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class Claw{
 
-    // DoubleSolenoid armExtension = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
     DoubleSolenoid clawLeft = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
     DoubleSolenoid clawRight = new DoubleSolenoid(PneumaticsModuleType.REVPH, 4, 5);
-    
-    // CANSparkMax armMotor = new CANSparkMax(7, MotorType.kBrushless);
-    // RelativeEncoder armMotorEncoder = armMotor.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
-    
+        
     Joystick joystick = new Joystick(0);
     int armState = 0;
     double armPosition = 0;
@@ -34,47 +30,12 @@ public class Claw{
     public void robotIntial() {
 
         //sets default states to the Solenoids
-        // armExtension.set(kReverse);
         clawLeft.set(kReverse);
         clawRight.set(kReverse);
 
     }
-
-    // Arm Extension control
-    // public void ArmExtension() {
-    //     armExtension.toggle();
-    // }
-
-    // public void joystickControl() {
-    //     //Arm Motor control
-    //     if (joystick.getRawButton(1) == true) {
-    //         armMotor.set(1);
-    //         armState = 0;
-    //     }
-    //     else if (joystick.getRawButton(2) == true) {
-    //         armMotor.set(-1);
-    //         armState = 0;
-    //     }
-    //     else {
-    //         armState ++;
-    //         if (armState == 1) {
-    //             armPosition = armMotorEncoder.getPosition();
-    //         }
-    //         armStall();
-    //     }
-    // } 
     
-    // public void armStall() {
-    //     double error = armPosition - armMotorEncoder.getPosition();
-    //     double speed = 0;
-    //     double k = 0.01;
-    //     if (error > 1 || error < -1) {
-    //         speed = error * k;
-    //     }
-    //     armMotor.set(speed);
-    // }
-    
-    // // Claw Solenoid control
+    // Claw Solenoid control
     public void clawToggle() { 
         clawLeft.toggle();
         clawRight.toggle();
