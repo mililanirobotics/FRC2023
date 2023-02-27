fa12da4dad93a4f7817b659bfa91c4b2f8994979
unknown <123456789goldi@gmail.com>
2023-02-26 16:56:56 -1000

    refined autopath values

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -2,12 +2,12 @@ package frc.robot;
 
 public class Auto {
     
-    private static final String kDockAndEngage1 = "Dock and Engage Left";
-    private static final String kDockAndEngage2 = "Dock and Engage Middle";
-    private static final String kDockAndEngage3 = "Dock and Engage Right";
-    private static final String kScoring1 = "Scoring on the Left";
-    private static final String kScoring2 = "Scoring in the Middle";
-    private static final String kScoring3 = "Scoring on the Right";
+    private static final String kDockAndEngage1 = "Score then Dock and Engage Left";
+    private static final String kDockAndEngage2 = "Score then Dock and Engage Middle";
+    private static final String kDockAndEngage3 = "Score then Dock and Engage Right";
+    private static final String kScoring1 = "Scoring only Left";
+    private static final String kScoring2 = "Scoring only Middle";
+    private static final String kScoring3 = "Scoring only Right";
 
     Drive drive = new Drive();
     PivotArm pivotArm = new PivotArm();
@@ -16,76 +16,68 @@ public class Auto {
     public void autonomous(String autopath)
     {
         switch(autopath) {
-            case kDockAndEngage1:
-            drive.encoderDrive(0.3, 12, "Forward", 1000);
-            pivotArm.setArmControl(35);
-            claw.clawToggle();
-            drive.encoderDrive(0.4, 136.5, "Backward", 5000);
-            pivotArm.setArmControl(0);
-            drive.turnDrive(0.13, -90, 1500);
-            drive.encoderDrive(0.3, 36, "Forward", 3000);
-            drive.turnDrive(0.13, 90, 1500);
-            drive.encoderDrive(0.3, 40, "Forward", 3000);
-            // Dock and Engage code
+            case kDockAndEngage1: //scoring preload then balance and engage in left position
+            pivotArm.setArmControl(40); //set arm to score
+            claw.clawToggle(); //opens and close claw to score
+            drive.encoderDrive(0.5, 198, "Backward", 5000); //drive backwards 16.5 ft for mobility
+            pivotArm.setArmControl(0); //puts arm down
+            drive.turnDrive(0.13, -90, 1500); //turn robot left 90 degrees (relative to starting position)
+            drive.encoderDrive(0.3, 60, "Forward", 3000); //drives foward 5ft
+            drive.turnDrive(0.13, 90, 1500); //turn robot right 90 degrees
+            drive.encoderDrive(0.3, 36, "Forward", 3000); //drives foward 3 ft
+            //start of balancing method 
+            
             break;
-          case kDockAndEngage2:
-            drive.encoderDrive(0.3, 12, "Forward", 1000);
-            pivotArm.setArmControl(30);
-            claw.clawToggle();
-            drive.encoderDrive(0.3, 20, "Backward", 1500);
-            pivotArm.setArmControl(0);
-            drive.turnDrive(0.13, 180, 2000);
-            drive.encoderDrive(0.4, 35, "Forward", 3500);
-            // Dock and Engage code
+          case kDockAndEngage2: //scoring preload then balance and engage in mid position
+            pivotArm.setArmControl(40); //set arm to score
+            claw.clawToggle(); //opens and close claw to score
+            drive.encoderDrive(0.5, 200, "Backward", 1500); //drive backwards 16.5 ft (+2 inches to account for going over charge station)
+            pivotArm.setArmControl(0); //puts arm down
+            // drive.turnDrive(0.13, 180, 2000);
+            drive.encoderDrive(0.4, 36, "Forward", 3500); //drive foward heading to balance on charge station
+            //start of balancing method
+
             break;
-          case kDockAndEngage3:
-            drive.encoderDrive(0.3, 12, "Forward", 1000);
-            pivotArm.setArmControl(35);
-            claw.clawToggle();
-            drive.encoderDrive(0.4, 136.5, "Backward", 5000);
-            pivotArm.setArmControl(0);
-            drive.turnDrive(0.13, 90, 1500);
-            drive.encoderDrive(0.3, 36, "Forward", 3000);
-            drive.turnDrive(0.13, -90, 1500);
-            drive.encoderDrive(0.3, 40, "Forward", 3000);
-            // Dock and Engage code
+          case kDockAndEngage3: //scoring preload then balance and engage in right position
+            pivotArm.setArmControl(40); //set arm to score
+            claw.clawToggle(); //opens and close claw to score
+            drive.encoderDrive(0.5, 200, "Backward", 5000); //drive backwards 16.5 ft for mobility
+            pivotArm.setArmControl(0); //puts arm down
+            drive.turnDrive(0.13, 90, 1500); //turns 90 degree right (relative to robot starting position)
+            drive.encoderDrive(0.3, 60, "Forward", 3000); //drives foward 5ft
+            drive.turnDrive(0.13, -90, 1500); //turn 90 degree left to face charge station
+            drive.encoderDrive(0.3, 36, "Forward", 3000); //drive foward heading to balance on charge station
+            //start of balancing method
+
             break;
-          case kScoring1:
-            drive.encoderDrive(0.3, 12, "Forward", 1000);
-            pivotArm.setArmControl(35);
-            claw.clawToggle();
-            drive.encoderDrive(0.4, 136.5, "Backward", 5000);
-            pivotArm.setArmControl(0);
-            drive.turnDrive(0.13, -90, 1500);
-            drive.encoderDrive(0.3, 24, "Forward", 3000);
-            drive.turnDrive(0.13, -90, 1500);
-            drive.encoderDrive(0.3, 36, "Forward", 3000);
+          case kScoring1: //scoring only in left position
+            pivotArm.setArmControl(40); //set arm to score
+            claw.clawToggle(); //opens and close claw to score
+            drive.encoderDrive(0.4, 198, "Backward", 5000); //drive backwards 16.5 ft 
+            pivotArm.setArmControl(0); //puts arm down
+            drive.turnDrive(0.15, 185, 1500); //turns robot 180 degrees right relative to starting position (+5 for game piece heading)
+            drive.encoderDrive(0.3, 36, "Forward", 3000); //drives 3ft foward approaching game piece
             break;
-          case kScoring2:
-            drive.encoderDrive(0.3, 12, "Forward", 1000);
-            pivotArm.setArmControl(30);
-            claw.clawToggle();
-            drive.encoderDrive(0.3, 20, "Backward", 1500);
-            pivotArm.setArmControl(0);
-            drive.turnDrive(0.13, -90, 1500);
-            drive.encoderDrive(0.3, 30, "Forward", 3500);
-            drive.turnDrive(0.13, -90, 1500);
-            drive.encoderDrive(0.3, 36, "Forward", 3000);
-            drive.turnDrive(0.13, -90, 1500);
-            drive.encoderDrive(0.3, 36, "Forward", 3000);
-            drive.turnDrive(0.13, 90, 1500);
-            drive.encoderDrive(0.3, 40, "Forward", 4000);
+
+          case kScoring2: //scoring only in mid position
+            pivotArm.setArmControl(40); //set arm to score
+            claw.clawToggle(); //opens and close claw to score
+            drive.encoderDrive(0.4, 200, "Backward", 1500); //drive backwards 16.5 ft (+2 inches to account for going over charge station)
+            pivotArm.setArmControl(0); //puts arm down
+            drive.turnDrive(0.13, -90, 1500); //turns 90 degree left (relative to starting position)
+            drive.encoderDrive(0.3, 30, "Forward", 3500); //drives foward
+            drive.turnDrive(0.13, -90, 1500); //turns 90 degree left 
+            drive.encoderDrive(0.3, 60, "Forward", 3000); //drives 5ft foward approaching game piece
             break;
-          case kScoring3:
-            drive.encoderDrive(0.3, 12, "Forward", 1000);
-            pivotArm.setArmControl(35);
-            claw.clawToggle();
-            drive.encoderDrive(0.4, 136.5, "Backward", 5000);
-            pivotArm.setArmControl(0);
-            drive.turnDrive(0.13, 90, 1500);
-            drive.encoderDrive(0.3, 20, "Forward", 2000);
-            drive.turnDrive(0.13, 90, 1500);
-            drive.encoderDrive(0.3, 40, "Forward", 3000);
+
+          case kScoring3: //scoring only in right position
+            pivotArm.setArmControl(40); //set arm to score
+            claw.clawToggle(); //opens and close claw to score
+            drive.encoderDrive(0.4, 200, "Backward", 5000); //drive backwards 16.5 ft 
+            pivotArm.setArmControl(0); //puts arm down
+            drive.turnDrive(0.13, 185, 1500); //turns robot 180 degrees right relative to stating positon (+5 for game piece heading)
+            drive.encoderDrive(0.3, 36, "Forward", 2000); //drives 3ft foward approaching game piece
+            break;
         }
     }
 }
